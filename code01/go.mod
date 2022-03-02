202e948f22563ae67fc7710093ab58c96dee8393
windego <zhangwj0520@hotmail.com>
2022-03-02 17:52:29 +0800

    变量&常量

--- code01/go.mod
+++ code01/go.mod
@@ -1,3 +1,5 @@
 module windego.cn/code01
 
 go 1.17
+
+require github.com/google/go-cmp v0.5.7 // indirect
--- code01/test.go
+++ code01/test.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/google/go-cmp/cmp"
+	"windego.cn/code01/morestrings"
+)
 
 func main() {
 	fmt.Println("hello world")
+	s := morestrings.ReverseRunes("test")
+	fmt.Printf("s: %v\n", s)
+
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
